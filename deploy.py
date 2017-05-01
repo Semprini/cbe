64e7a77f2e118e93d0841ac7ceda9e43a44bf77a
Paul Atkin <paul@sphinx.co.nz>
2017-05-02 11:52:12 +1200

    Fixed deploy.py path to cbe app

---
+++ deploy.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import django
+
+# Find the path to the current file and add the path to cbe to the system path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(BASE_DIR, 'cbe')) #cbe is a sub directory of this file
+
+# Initialize django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cbe.settings'
+django.setup()
+
+# Create a superuser
+from django.contrib.auth.models import User
+superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
