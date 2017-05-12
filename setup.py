97bb4dcd1c6662ac13a84cbb803bd573a2d72233
Paul Atkin <paul@sphinx.co.nz>
2017-05-12 16:41:25 +1200

    Minor variable name fixes

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-import os, sys, glob, fnmatch
+import os
 
 from distutils.core import setup
 
 def find_packages(srcdir):
-    path_list = []
+    package_list = []
     badnames=["__pycache__",]
     for root, dirs, files in os.walk(srcdir):
         if not any(bad in root for bad in badnames):
             if "__init__.py" in files:
-                path_list.append( root.replace("/",".").replace("\\",".").strip('.') )
-    return path_list
+                package_list.append( root.replace("/",".").replace("\\",".").strip('.') )
+    return package_list
 
 cbe_packages = find_packages('cbe/')
 
