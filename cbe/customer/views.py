e21e6781488e5b82e68995942862f59dd26dd61f
Semprini <atkinpa@gmail.com>
2017-08-14 17:47:24 +1200

    Added app router

--- cbe/customer/views.py
+++ cbe/customer/views.py
@@ -20,25 +20,24 @@ class CustomerViewSet(viewsets.ModelViewSet):
     #                      IsOwnerOrReadOnly,)
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
-            return Customer.objects.all()
-        else:
-            # Check if the user has an indiviual object, if not return unmanaged customers only
-            individuals = Individual.objects.filter(user=self.request.user)
-            if len( individuals ) != 1:
-                return Customer.objects.filter(managed_by=None)
+        if not self.request.user.is_anonymous:# == AnonymousUser:
+            if self.request.user.is_superuser:
+                return Customer.objects.all()
+            else:
+                # Check if the user has an indiviual object, if not return unmanaged customers only
+                individuals = Individual.objects.filter(user=self.request.user)
+                if len( individuals ) != 1:
+                    return Customer.objects.filter(managed_by=None)
+                    
+                # Find organisations where the individual is on staff
+                staffs = Staff.objects.filter( party_content_type__model='individual', party_object_id=individuals[0].pk )
+                organisations = []
+                for staff in staffs:
+                    organisations.append( staff.company )
                 
-            # Find organisations where the individual is on staff
-            staffs = Staff.objects.filter( party_content_type__model='individual', party_object_id=individuals[0].pk )
-            organisations = []
-            for staff in staffs:
-                organisations.append( staff.company )
-            
-            return Customer.objects.filter( Q(managed_by__in=organisations) | Q(managed_by=None) )
-        
-        
-    # def perform_create(self, serializer):
-    #    serializer.save(owner=self.request.user)
+                return Customer.objects.filter( Q(managed_by__in=organisations) | Q(managed_by=None) )
+        else:
+            return []   #TODO: Auth Exception
 
 
 class CustomerAccountViewSet(viewsets.ModelViewSet):
--- cbe/urls.py
+++ cbe/urls.py
@@ -22,6 +22,7 @@ from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 
+from cbe.routers import AppRouter
 from . import views
 
 import cbe.party.views as PartyViews
@@ -69,74 +70,101 @@ class ContentTypeViewSet(viewsets.ModelViewSet):
     queryset = ContentType.objects.all()
     serializer_class = ContentTypeSerializer
 
-cberouter = DefaultRouter()
-cberouter.register(r'party/individual', PartyViews.IndividualViewSet)
-cberouter.register(r'party/organisation', PartyViews.OrganisationViewSet)
-cberouter.register(
-    r'party/generic_party_role', PartyViews.GenericPartyRoleViewSet)
-cberouter.register(
-    r'party/telephone_number', PartyViews.TelephoneNumberViewSet)
-cberouter.register(r'party/owner', PartyViews.OwnerViewSet)
-cberouter.register(r'party/party_role_association', PartyViews.PartyRoleAssociationViewSet)
+cberouter = AppRouter()
 
-cberouter.register(r'location/country', LocationViews.CountryViewSet)
-cberouter.register(r'location/city', LocationViews.CityViewSet)
-cberouter.register(
-    r'location/urban_property_address', LocationViews.UrbanPropertyAddressViewSet)
-cberouter.register(
-    r'location/po_box_address', LocationViews.PoBoxAddressViewSet)
-cberouter.register(
-    r'location/location', LocationViews.LocationViewSet)
+partyrouter = AppRouter(root_view_name='app-party')
+partyrouter.register(r'individual', PartyViews.IndividualViewSet)
+partyrouter.register(r'organisation', PartyViews.OrganisationViewSet)
+partyrouter.register(
+    r'generic_party_role', PartyViews.GenericPartyRoleViewSet)
+partyrouter.register(
+    r'telephone_number', PartyViews.TelephoneNumberViewSet)
+partyrouter.register(r'owner', PartyViews.OwnerViewSet)
+partyrouter.register(r'party_role_association', PartyViews.PartyRoleAssociationViewSet)
 
-cberouter.register(r'human_resources/staff', HumanResourcesViews.StaffViewSet)
-cberouter.register(r'human_resources/identification', HumanResourcesViews.IdentificationViewSet)
-cberouter.register(r'human_resources/identification_type', HumanResourcesViews.IdentificationTypeViewSet)
+locationrouter = AppRouter(root_view_name='app-location')
+locationrouter.register(r'country', LocationViews.CountryViewSet)
+locationrouter.register(r'city', LocationViews.CityViewSet)
+locationrouter.register(
+    r'urban_property_address', LocationViews.UrbanPropertyAddressViewSet)
+locationrouter.register(
+    r'po_box_address', LocationViews.PoBoxAddressViewSet)
+locationrouter.register(
+    r'location', LocationViews.LocationViewSet)
 
-cberouter.register(r'resource/physical_resource', ResourceViews.PhysicalResourceViewSet)
+humanresourcesrouter = AppRouter(root_view_name='app-human_resources')
+humanresourcesrouter.register(r'staff', HumanResourcesViews.StaffViewSet)
+humanresourcesrouter.register(r'identification', HumanResourcesViews.IdentificationViewSet)
+humanresourcesrouter.register(r'identification_type', HumanResourcesViews.IdentificationTypeViewSet)
+
+physicalresourcerouter = AppRouter(root_view_name='app-physical_resource')
+physicalresourcerouter.register(r'physical_resource', ResourceViews.PhysicalResourceViewSet)
 
 
-cberouter.register(r'customer/customer', CustomerViews.CustomerViewSet, base_name='customer')
-cberouter.register(r'customer/account', CustomerViews.CustomerAccountViewSet)
-cberouter.register(r'customer/customer_account_contact',
+customerrouter = AppRouter(root_view_name='app-customer')
+customerrouter.register(r'customer', CustomerViews.CustomerViewSet, base_name='customer')
+customerrouter.register(r'account', CustomerViews.CustomerAccountViewSet)
+customerrouter.register(r'customer_account_contact',
                    CustomerViews.CustomerAccountContactViewSet)
 
-cberouter.register(r'credit/credit', CreditViews.CreditViewSet)
-cberouter.register(r'credit/credit_balance_event', CreditViews.CreditBalanceEventViewSet)
-cberouter.register(r'credit/credit_profile', CreditViews.CreditProfileViewSet)
+creditrouter = AppRouter(root_view_name='app-credit')
+creditrouter.register(r'credit', CreditViews.CreditViewSet)
+creditrouter.register(r'credit_balance_event', CreditViews.CreditBalanceEventViewSet)
+creditrouter.register(r'credit_profile', CreditViews.CreditProfileViewSet)
+      
+troublerouter = AppRouter(root_view_name='app-trouble')
+troublerouter.register(r'problem', TroubleViews.ProblemViewSet)
 
-                   
-cberouter.register(r'trouble/problem', TroubleViews.ProblemViewSet)
+physicalobjectrouter = AppRouter(root_view_name='app-physical_object')
+physicalobjectrouter.register(r'structure', PhysicalObjectViews.StructureViewSet)
+physicalobjectrouter.register(r'vehicle', PhysicalObjectViews.VehicleViewSet)
+physicalobjectrouter.register(r'device', PhysicalObjectViews.DeviceViewSet)
 
-cberouter.register(r'physical_object/structure', PhysicalObjectViews.StructureViewSet)
-cberouter.register(r'physical_object/vehicle', PhysicalObjectViews.VehicleViewSet)
-cberouter.register(r'physical_object/device', PhysicalObjectViews.DeviceViewSet)
+supplierpartnerrouter = AppRouter(root_view_name='app-supplier_partner')
+supplierpartnerrouter.register(r'supplier', SupplierPartnerViews.SupplierViewSet)
+supplierpartnerrouter.register(r'partner', SupplierPartnerViews.PartnerViewSet)
+supplierpartnerrouter.register(r'buyer', SupplierPartnerViews.BuyerViewSet)
 
-cberouter.register(r'supplier_partner/supplier', SupplierPartnerViews.SupplierViewSet)
-cberouter.register(r'supplier_partner/partner', SupplierPartnerViews.PartnerViewSet)
-cberouter.register(r'supplier_partner/buyer', SupplierPartnerViews.BuyerViewSet)
+informationtechnologyrouter = AppRouter(root_view_name='app-information_technology')
+informationtechnologyrouter.register(r'component', ITViews.ComponentViewSet)
+informationtechnologyrouter.register(r'component', ITViews.ComponentViewSet)
+informationtechnologyrouter.register(r'component_classification', ITViews.ComponentClassificationViewSet)
+informationtechnologyrouter.register(r'process_framework', ITViews.ProcessFrameworkViewSet)
+informationtechnologyrouter.register(r'process', ITViews.ProcessViewSet)
+informationtechnologyrouter.register(r'process_classification', ITViews.ProcessClassificationViewSet)
 
-cberouter.register(r'information_technology/component', ITViews.ComponentViewSet)
-cberouter.register(r'information_technology/component_classification', ITViews.ComponentClassificationViewSet)
-cberouter.register(r'information_technology/process_framework', ITViews.ProcessFrameworkViewSet)
-cberouter.register(r'information_technology/process', ITViews.ProcessViewSet)
-cberouter.register(r'information_technology/process_classification', ITViews.ProcessClassificationViewSet)
+projectrouter = AppRouter(root_view_name='app-project')
+projectrouter.register(r'project', ProjectViews.ProjectViewSet)
 
-cberouter.register(r'project/project', ProjectViews.ProjectViewSet)
-
-router = DefaultRouter()
+router = AppRouter( apps={  'party':'app-party',
+                            'location':'app-location',
+                            'human_resources':'app-human_resources',
+                            'physical_resource':'app-physical_resource',
+                            'customer':'app-customer',
+                            'credit':'app-credit',
+                            'trouble':'app-trouble',
+                            'physical_object':'app-physical_object',
+                            'supplier_partner':'app-supplier_partner',
+                            'information_technology':'app-information_technology', } )
 router.register(r'auth/users', UserViewSet)
 router.register(r'content_types', ContentTypeViewSet)
 
-for route in cberouter.registry:
-    if len(route) == 2:
-        router.register(route[0], route[1])
-    else:
-        router.register(route[0], route[1], base_name=route[2])
+appurlpatterns = [
+    url(r'^api/party/', include(partyrouter.urls)),
+    url(r'^api/location/', include(locationrouter.urls)),
+    url(r'^api/human_resources/', include(humanresourcesrouter.urls)),
+    url(r'^api/physical_resource/', include(physicalresourcerouter.urls)),
+    url(r'^api/customer/', include(customerrouter.urls)),
+    url(r'^api/credit/', include(creditrouter.urls)),
+    url(r'^api/trouble/', include(troublerouter.urls)),
+    url(r'^api/physical_object/', include(physicalobjectrouter.urls)),
+    url(r'^api/supplier_partner/', include(supplierpartnerrouter.urls)),
+    url(r'^api/information_technology/', include(informationtechnologyrouter.urls)),
+]
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')),
-]
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+] + appurlpatterns
