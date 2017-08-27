0a685c8ef2919c5dd2b001df6fe6a2d6aa90cddd
Semprini <atkinpa@gmail.com>
2017-08-28 11:41:45 +1200

    Minor meta changes on supplier partner

--- cbe/supplier_partner/models.py
+++ cbe/supplier_partner/models.py
@@ -3,17 +3,29 @@ from django.db import models
 from cbe.party.models import PartyRole
 
 class Supplier(PartyRole):
+    class Meta:
+        ordering = ['id']
+
     def save(self, *args, **kwargs):
-        self.name = "Supplier"
+        if self.name == "":
+            self.name = "Supplier"
         super(Supplier, self).save(*args, **kwargs)
 
 class Partner(PartyRole):
+    class Meta:
+        ordering = ['id']
+
     def save(self, *args, **kwargs):
-        self.name = "Partner"
+        if self.name == "":
+            self.name = "Partner"
         super(Partner, self).save(*args, **kwargs)
 
 class Buyer(PartyRole):
+    class Meta:
+        ordering = ['id']
+
     def save(self, *args, **kwargs):
-        self.name = "Buyer"
+        if self.name == "":
+            self.name = "Buyer"          
         super(Buyer, self).save(*args, **kwargs)
         
