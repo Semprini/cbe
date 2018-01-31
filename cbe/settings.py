8ba6c8264a94552dadb88d517023152618d20f89
Paul Atkin <paul@sphinx.co.nz>
2018-01-31 15:36:09 +1300

    Restart migrations

--- cbe/settings.py
+++ cbe/settings.py
@@ -105,6 +105,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissions'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_RENDERER_CLASSES': (
