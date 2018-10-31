b017b54adbce31151f14a293e933e72ca905e683
Jules Clements <jules.clements@healthalliance.co.nz>
2018-11-01 07:59:04 +1300

    Apply expression debug line

--- automation/remote/execute.ps1
+++ automation/remote/execute.ps1
@@ -15,12 +15,12 @@ function executeExpression ($expression) {
 		$ExecutionContext.InvokeCommand.ExpandString($expression)
     }
 	try {
-		$output = Invoke-Expression $expression
+		Write-Host "$expression"
+		Invoke-Expression $expression
 	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
 	} catch { echo $_.Exception|format-list -force; exit 2 }
     if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
     if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
-    return $output
 }
 
 function MAKDIR ($itemPath) { 
